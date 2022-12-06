db44ef376503a4aaaadb626b7faf3055ee43bb58
angieli13 <angiequimica13@gmail.com>
2022-12-06 05:43:49 -0500

    porFin

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import validator from './validator.js';
 const queryString = window.location.search.substring(0);
 const urlParams = new URLSearchParams(queryString);
 
-const textin = document.getElementById('texton').value;
-alert("1 ",queryString);
-alert("2 ",urlParams.get('texton'));
-document.getElementById("boton").addEventListener("click", validator.isValid(textin));
+//const numerosTarjeta = textin.value   
+document.getElementById("boton").addEventListener("click", function(){
+    const numerosTarjeta = document.getElementById('texton').value; 
+    console.log(numerosTarjeta);
+    if ( validator.isValid(numerosTarjeta)==true){
+        alert ("tarjeta valida")
+        document.getElementById('texton').value = validator.maskify(numerosTarjeta);
+    } else {
+        alert ("tarjeta invalida")
+    }
+ });
+
 //EventTargetinterfaz configura una función que se llamará cada vez que el evento especificado se envíe al destino.
- 
+ //validator.isValid(textin)
--- src/validator.js
+++ src/validator.js
@@ -2,20 +2,24 @@ const validator = {
     
   isValid: function(cardNumber) {
     console.log("boton funciona");
+  
     if (cardNumber.length >= 15){
       
       const array = []; // Se crea un array vacío en donde se acomodarán los números de la tarjeta.
 
       for(let i = cardNumber.length -1; i >= 0; i--){
+
         array.push(parseInt(cardNumber[i]));
+        
       }
       
       let sumanumero = 0;
 
       for(let p = 0; p <= array.length-1; p++){ // Se itera en los números de las posiciones pares.
+        
         if ((p%2) === 0) {
           const mult = array[p] * 2; // Se multiplican por 2 los números de las posiciones pares.
-    
+          
           if (mult > 9){ // Si el resultado de la multiplicación tiene 2 dígitos, se deben sumar esos dígitos.
             sumanumero = sumanumero + (Number(mult.toString().charAt(0)) + Number(mult.toString().charAt(1)));
           }else{
@@ -26,14 +30,11 @@ const validator = {
         }            
       }
 
-      //console.log({array, sumanumero});
       //tarjeta valida 
-      if(sumanumero.toString().charAt(1)===0){
-        //console.log("ok", sumanumero);
+      if(Number(sumanumero.toString().charAt(1)) == 0){
         //alert("tarjeta valida");
         return true;
       } else {
-        //console.log("error",sumanumero);
         //alert("tarjeta no valida");
         return false;
       }
